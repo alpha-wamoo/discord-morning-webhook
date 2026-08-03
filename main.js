b3f5b9a85cd1b36dbf0ca024a9d0b2a04d5f2aeb
alpha-wamoo <uuvfo90@gmail.com>
2026-08-03 18:59:08 +0900

    aaws

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import OpenAI from "./modules/OpenAI.js";
 import Discord from "./modules/Discord.js";
 
+console.log("Secretsの初期化開始");
 /**@type {{[key: string]: string}} */
-const {GPT_API_KEY, DISCORD_WEBHOOK_GENERAL, DISCORD_WEBHOOK_PHRASE, X_CLIENT_ID, X_CLIENT_SECRET} = process.env;
+const {GPT_API_KEY, GPT_API_KEY_V2, DISCORD_WEBHOOK_GENERAL, DISCORD_WEBHOOK_PHRASE, X_CLIENT_ID, X_CLIENT_SECRET} = process.env;
+if(!GPT_API_KEY_V2 || !DISCORD_WEBHOOK_GENERAL || !DISCORD_WEBHOOK_PHRASE){
+  console.error("One or more required environment variables are not set.");
+  process.exit(1);
+}
+console.log("Secretsの初期化完了");
 
 (async function(){
-  const ai = new OpenAI(GPT_API_KEY);
+  const ai = new OpenAI(GPT_API_KEY_V2);
   const reply = await ai.call(
     { role: "system", content: "真剣で情熱的、社交的でテンションが高め、感情豊かで素直、協調性があり好奇心旺盛な大学生。絵文字、ビックリマーク、伸ばし棒を多用。ボケもツッコミも好き。「おはよう」ではなく「おはおはよう」と言う。誇張した奇抜な比喩表現が大好き。二人称は「あなた」。IRIAMでライバーをしている。" },
     { role: "user", content: "インターネットの友達に送るための朝の挨拶を130文字程度で考えて。たまにめっちゃふざけた挨拶になる" }
@@ -23,7 +29,7 @@ const {GPT_API_KEY, DISCORD_WEBHOOK_GENERAL, DISCORD_WEBHOOK_PHRASE, X_CLIENT_ID
     console.log(`webhook送信完了: ${res.status} ${res.statusText}`);
   }
 
-  console.log("successfly sent all webhooks.");
+  console.log("All successfully sent!");
   return null;
 })().catch(err => {
   console.log(`webhook送信失敗: ${err}`);
